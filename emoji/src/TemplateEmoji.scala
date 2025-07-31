2bcf5fc9924918f7731624ecef13e780216032c3
snoggles <191955932+snoggles@users.noreply.github.com>
2025-07-30 23:17:50 -0400

    Remove var/mutability.

--- emoji/src/TemplateEmoji.scala
+++ emoji/src/TemplateEmoji.scala
@@ -9,7 +9,7 @@ object TemplateEmoji:
   private val inputPath = imagesPath / "input"
   private val outputPath = imagesPath / "output"
   private val purpleBgImg = ImmutableImage.loader().fromFile((imagesPath / "purple-bg.png").toIO)
-  private val composite = AlphaComposite(1)
+  private val overlay = AlphaComposite(1)
 
   @main def run(): Unit =
     for
@@ -24,28 +24,7 @@ object TemplateEmoji:
       emojiImage.output(PngWriter.MaxCompression, destPath.toIO)
 
   private def emojifyImage(source: ImmutableImage): ImmutableImage =
-    // Trim the whitespace
-    var img = cropTight(source)
-    val perkSize = math.max(img.width, img.height)
-    img = img.resizeTo(perkSize, perkSize, Position.Center)
-
-    // Apply the purple background
-    img = purpleBgImg.scaleTo(perkSize, perkSize).composite(composite, img)
-
-    // Add a black border
-    val borderThickness = math.round(0.05 * perkSize).toInt
-    val compositeSize = perkSize + borderThickness * 2
-    val blackBg = ImmutableImage.create(compositeSize, compositeSize).fill(Color.black)
-    img = img.resizeTo(compositeSize, compositeSize, Position.Center)
-    img = blackBg.composite(composite, img)
-    img
-
-  private def toEmojiName(fileName: String): String =
-    fileName
-      .replaceAll("[^A-Za-z ]", "")
-      .split("""\s+""")
-      .map(_.toLowerCase.capitalize)
-      .mkString("z", "", "")
+    addBlackBorder(addPurpleBg(padToSquare(cropTight(source))))
 
   private def cropTight(img: ImmutableImage): ImmutableImage =
     val xs = 0 until img.width
@@ -58,9 +37,32 @@ object TemplateEmoji:
 
     val xLeft = xs.find(x => ys.exists(y => isPerkPixel(x, y))).get
     val yTop = ys.find(y => xs.exists(x => isPerkPixel(x, y))).get
+    val xRight = xs.findLast(x => ys.exists(y => isPerkPixel(x, y))).get
+    val yBottom = ys.findLast(y => xs.exists(x => isPerkPixel(x, y))).get
 
-    val xRight = xs.reverseIterator.find(x => ys.exists(y => isPerkPixel(x, y))).get
-    val yBottom = ys.reverseIterator.find(y => xs.exists(x => isPerkPixel(x, y))).get
+    val width = xRight - xLeft + 1
+    val height = yBottom - yTop + 1
 
-    img.subimage(xLeft, yTop, xRight - xLeft + 1, yBottom - yTop + 1)
+    img.subimage(xLeft, yTop, width, height)
 
+  private def padToSquare(img: ImmutableImage): ImmutableImage =
+    val perkSize = math.max(img.width, img.height)
+    img.resizeTo(perkSize, perkSize, Position.Center)
+
+  private def addPurpleBg(img: ImmutableImage): ImmutableImage =
+    purpleBgImg.scaleTo(img.width, img.height).composite(overlay, img)
+
+  private def addBlackBorder(img: ImmutableImage): ImmutableImage =
+    require(img.width == img.height)
+    val borderThickness = math.round(0.05 * img.width).toInt
+    val compositeSize = img.width + borderThickness * 2
+    val blackBg = ImmutableImage.create(compositeSize, compositeSize).fill(Color.black)
+    val padded = img.resizeTo(compositeSize, compositeSize, Position.Center)
+    blackBg.composite(overlay, img)
+
+  private def toEmojiName(fileName: String): String =
+    fileName
+      .replaceAll("[^A-Za-z ]", "")
+      .split("""\s+""")
+      .map(_.toLowerCase.capitalize)
+      .mkString("z", "", "")
